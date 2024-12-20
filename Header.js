4fcfddfa8400e3f51f740aad8ca72650a16c565d
Lokesh <loki944818@gmail.com>
2024-12-20 15:33:18 +0530

    Header.js

---
+++ Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo">CodeAnt AI</div>
+      <nav>
+        <ul className="nav-links">
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
